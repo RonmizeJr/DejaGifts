98525612246cbec1fd97705111ffcf3e82ba4b57
Ronald Mize Jr <rwthojr5@gmail.com>
2024-07-24 14:34:59 -0500

    changing header component

--- src/components/shared/header/index.tsx
+++ src/components/shared/header/index.tsx
@@ -1,9 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { ShoppingCart, UserIcon } from 'lucide-react';
 import { APP_NAME } from '@/lib/constants';
-import { Button } from '@/components/ui/button';
-import Menu from './menu';
 
 const Header = async () => {
   return (
