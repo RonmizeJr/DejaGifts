410436220de03fce8384dac3f05cc91cd6a42efd
Ronald Mize Jr <rwthojr5@gmail.com>
2024-07-24 01:01:17 -0500

    implementing auth using NextAuth

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -2,3 +2,7 @@ export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Dejagifts';
 export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
   'An ecommerce application made with nextjs, pg, and shadcn-ui';
+export const signInDefaultValues = {
+  email: '',
+  password: '',
+};
